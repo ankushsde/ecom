b4d31af826fcb89c976177d329b6a41977dff38f
ankushsde <ankushs5252@gmail.com>
2024-01-31 04:47:41 +0530

    product detail page added

--- src/features/product-list/components/ProductList.js
+++ src/features/product-list/components/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useState, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, incrementAsync, selectCount } from "./ProductListSlice";
+import { increment, incrementAsync, selectCount } from "../ProductSlice";
 
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
@@ -12,6 +12,7 @@ import {
   Squares2X2Icon,
 } from "@heroicons/react/20/solid";
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
+import { Link } from "react-router-dom";
 
 const items = [
   { id: 1, title: 'Back End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
@@ -418,6 +419,7 @@ export default function ProductList() {
                       <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
                         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                           {products.map((product) => (
+                            <Link to="/product-detail">
                             <div key={product.id} className="group relative">
                               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                                 <img
@@ -446,6 +448,7 @@ export default function ProductList() {
                                 </p>
                               </div>
                             </div>
+                            </Link>
                           ))}
                         </div>
                       </div>
